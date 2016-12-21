4e0c0111b641a074d208f3fe71fd76ff2a66b441
Saar Katz <kats.saar@gmail.com>
2016-12-22 00:20:09 +0200

    Updated test code.

--- src/TestTree.java
+++ src/TestTree.java
@@ -5,13 +5,20 @@ public class TestTree {
 
     public static void main(String[] args) {
         WAVLTree tree = new WAVLTree();
-        TestInsert(tree, 5, 0, "[1,5,1]");
-        TestInsert(tree, 3, 1, "[[1,3,1]1,5,2]");
-        TestInsert(tree, 13, 0, "[[1,3,1]1,5,1[1,13,1]]");
-        TestInsert(tree, 27, 2, "[[1,3,1]2,5,1[2,13,1[1,27,1]]]");
-        TestInsert(tree, 50, 3, "[[1,3,1]2,5,1[[1,13,1]1,27,1[1,50,1]]]");
-        TestDelete(tree, 27, 0, "[[1,3,1]2,5,1[[1,13,1]1,50,2]]");
-        TestDelete(tree, 50, 0, "[[1,3,1]2,5,2[1,13,1]]");
+        TestInsert(tree, 10, 0, "[1,10,1]");
+        TestInsert(tree, 20, 1, "[2,10,1[1,20,1]]");
+        TestInsert(tree, 30, 3, "[[1,10,1]1,20,1[1,30,1]]");
+        TestInsert(tree, 40, 2, "[[1,10,1]2,20,1[2,30,1[1,40,1]]]");
+        TestInsert(tree, 50, 3, "[[1,10,1]2,20,1[[1,30,1]1,40,1[1,50,1]]]");
+        TestInsert(tree, 25, 5, "[[[1,10,1]1,20,1[1,25,1]]1,30,1[2,40,1[1,50,1]]]");
+        System.out.println(tree);
+        tree = new WAVLTree();
+        TestInsert(tree, 50, 0, "[1,50,1]");
+        TestInsert(tree, 40, 1, "[[1,40,1]1,50,2]");
+        TestInsert(tree, 30, 3, "[[1,30,1]1,40,1[1,50,1]]");
+        TestInsert(tree, 20, 2, "[[[1,20,1]1,30,2]1,40,2[1,50,1]]");
+        TestInsert(tree, 10, 3, "[[[1,10,1]1,20,1[1,30,1]]1,40,2[1,50,1]]");
+        TestInsert(tree, 25, 5, "[[[1,10,1]1,20,1[1,25,1]]1,30,1[2,40,1[1,50,1]]]");
         System.out.println(tree);
         System.out.println("Done!");
     }
@@ -25,9 +32,10 @@ public class TestTree {
         try {
             numOperations = tree.insert(key, "");
         } catch (RuntimeException rte) {
-            throw new RuntimeException("[Error " + String.valueOf(testNum) + "]: Exception was " +
+            System.err.println("[Error " + String.valueOf(testNum) + "]: Exception was " +
                     "thrown during run of insert(" + String.valueOf(key) + ").\n" +
                     "Message:\n" + rte.toString());
+            throw rte;
         }
         if (numOperations != rebalancingOperations && (VERBOSE || verbose)) {
             System.err.println("[Warning " + String.valueOf(testNum) + "]: Number of rebalancing " +
@@ -37,18 +45,22 @@ public class TestTree {
         }
         String result = tree.toString();
         int length = Math.min(result.length(), expected.length());
-        for (int i = 0; i < length; i++) {
+        int i;
+        for (i = 0; i < length; i++) {
             if (result.charAt(i) != expected.charAt(i)) {
-                String message = "[Error " + String.valueOf(testNum) + "]: Resulting tree " +
-                        "after insert(" + String.valueOf(key) + ") is incorrect!\n\t" +
-                        result + "\n\t" + String.format("%" + String.valueOf(i+1) + "s\n\t", "^") +
-                        expected;
-                if (ERROR_ON_MISTAKE) {
-                    throw new RuntimeException(message);
-                }
-                else {
-                    System.err.println(message);
-                }
+                break;
+            }
+        }
+        if ((i < length && result.charAt(i) != expected.charAt(i)) || result.length() != expected.length()) {
+            String message = "[Error " + String.valueOf(testNum) + "]: Resulting tree " +
+                    "after insert(" + String.valueOf(key) + ") is incorrect!\n\t" +
+                    result + "\n\t" + String.format("%" + String.valueOf(i+1) + "s\n\t", "^") +
+                    expected;
+            if (ERROR_ON_MISTAKE) {
+                throw new RuntimeException(message);
+            }
+            else {
+                System.err.println(message);
             }
         }
         testNum++;
@@ -64,9 +76,10 @@ public class TestTree {
             numOperations = tree.delete(key);
         }
         catch (RuntimeException rte) {
-            throw new RuntimeException("[Error " + String.valueOf(testNum) + "]: Exception was " +
+            System.err.println("[Error " + String.valueOf(testNum) + "]: Exception was " +
                     "thrown during run of delete(" + String.valueOf(key) + ").\n" +
                     "Message:\n" + rte.toString());
+            throw rte;
         }
         if (numOperations != rebalancingOperations && (VERBOSE || verbose)) {
             System.err.println("[Warning " + String.valueOf(testNum) + "]: Number of rebalancing " +
@@ -76,18 +89,22 @@ public class TestTree {
         }
         String result = tree.toString();
         int length = Math.min(result.length(), expected.length());
-        for (int i = 0; i < length; i++) {
+        int i;
+        for (i = 0; i < length; i++) {
             if (result.charAt(i) != expected.charAt(i)) {
-                String message = "[Error " + String.valueOf(testNum) + "]: Resulting tree " +
-                        "after delete(" + String.valueOf(key) + ") is incorrect!\n\t\t" +
-                        result + "\n\t\t" + String.format("%" + String.valueOf(i+1) + "s\n\t\t", "^") +
-                        expected;
-                if (ERROR_ON_MISTAKE) {
-                    throw new RuntimeException(message);
-                }
-                else {
-                    System.err.println(message);
-                }
+                break;
+            }
+        }
+        if ((i < length && result.charAt(i) != expected.charAt(i)) || result.length() != expected.length()) {
+            String message = "[Error " + String.valueOf(testNum) + "]: Resulting tree " +
+                    "after delete(" + String.valueOf(key) + ") is incorrect!\n\t\t" +
+                    result + "\n\t\t" + String.format("%" + String.valueOf(i+1) + "s\n\t\t", "^") +
+                    expected;
+            if (ERROR_ON_MISTAKE) {
+                throw new RuntimeException(message);
+            }
+            else {
+                System.err.println(message);
             }
         }
         testNum++;
