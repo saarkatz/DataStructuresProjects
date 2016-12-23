711e91f9cd543d17261a0073c168a60cc6f2b428
Saar Katz <kats.saar@gmail.com>
2016-12-24 00:15:19 +0200

    Corrected and improved test code. Also added some more tests.

--- src/TestTree.java
+++ src/TestTree.java
@@ -1,9 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class TestTree {
-    static int testNum = 1;
+    private static int testNum = 1;
+    private static String testSuiteName = "";
+    private static boolean testSuiteFailed = false;
+    private static boolean testSuiteAborted = false;
+
     final static boolean VERBOSE = true;
     final static boolean ERROR_ON_MISTAKE = true;
 
     public static void main(String[] args) {
+        StartTestSuite("Insert 1");
         WAVLTree tree = new WAVLTree();
         TestInsert(tree, 10, 0, "[1,10,1]");
         TestInsert(tree, 20, 1, "[2,10,1[1,20,1]]"); // Case 1
@@ -14,6 +22,9 @@ public class TestTree {
         TestInsert(tree,  5, 3, "[[[[1,5,1]1,10,2]1,20,2[1,25,1]]1,30,2[2,40,1[1,50,1]]]"); // Case 1
         TestInsert(tree, 15, 0, "[[[[1,5,1]1,10,1[1,15,1]]1,20,2[1,25,1]]1,30,2[2,40,1[1,50,1]]]");
         TestInsert(tree, 27, 1, "[[[[1,5,1]1,10,1[1,15,1]]1,20,1[2,25,1[1,27,1]]]1,30,2[2,40,1[1,50,1]]]"); // Case 1
+        EndTestSuite();
+
+        StartTestSuite("Delete 1");
         TestDelete(tree, 25, 0, "[[[[1,5,1]1,10,1[1,15,1]]1,20,2[1,27,1]]1,30,2[2,40,1[1,50,1]]]");
         TestDelete(tree, 10, 0, "[[[2,5,1[1,15,1]]1,20,2[1,27,1]]1,30,2[2,40,1[1,50,1]]]");
         TestDelete(tree,  5, 0, "[[[1,15,1]2,20,2[1,27,1]]1,30,2[2,40,1[1,50,1]]]");
@@ -23,19 +34,28 @@ public class TestTree {
         TestInsert(tree, 17, 1, "[[2,15,1[1,17,1]]1,20,2[1,30,1]]");
         TestDelete(tree, 30, 3, "[[1,15,1]2,17,2[1,20,1]]"); // Case 4
         TestDelete(tree, 15, 1, "[2,17,1[1,20,1]]"); // Case 1
-        System.out.println(tree);
+        TestDelete(tree, 15, 1, "[1,20,1]"); // Case 1
+        TestDelete(tree, 15, 0, "[1,20,1]"); // Delete a node that does not exists.
+        TestDelete(tree, 20, 0, "[]"); // Delete last node.
+        TestDelete(tree, 20, 0, "[]"); // Delete when root is null;
+        EndTestSuite();
+
+        StartTestSuite("Insert 2");
         tree = new WAVLTree();
         TestInsert(tree, 50, 0, "[1,50,1]");
         TestInsert(tree, 40, 1, "[[1,40,1]1,50,2]"); // Case 1
         TestInsert(tree, 30, 3, "[[1,30,1]1,40,1[1,50,1]]"); // Case 2
         TestInsert(tree, 20, 2, "[[[1,20,1]1,30,2]1,40,2[1,50,1]]"); // Case 1
         TestInsert(tree, 10, 3, "[[[1,10,1]1,20,1[1,30,1]]1,40,2[1,50,1]]"); // Case 2
-        TestInsert(tree, 35, 5, "[[[1,10,1]1,20,2]1,30,1[[1,35,1]1,40,1[1,50,1]]"); // Case 3
-        TestInsert(tree, 55, 3, "[[[1,10,1]1,20,2]2,30,1[[1,35,1]2,40,1[2,50,1[1,55,1]]]"); // Case 1
-        TestInsert(tree, 45, 0, "[[[1,10,1]1,20,2]2,30,1[[1,35,1]2,40,1[[1,45,1]1,50,1[1,55,1]]]");
-        TestInsert(tree, 32, 1, "[[[1,10,1]1,20,2]2,30,1[[[1,32,1]1,35,2]1,40,1[[1,45,1]1,50,1[1,55,1]]]"); // Case 1
-        TestDelete(tree, 35, 0, "[[[1,10,1]1,20,2]2,30,1[[1,32,1]2,40,1[[1,45,1]1,50,1[1,55,1]]]");
-        TestDelete(tree, 50, 0, "[[[1,10,1]1,20,2]2,30,1[[1,32,1]2,40,1[2,45,1[1,55,1]]]");
+        TestInsert(tree, 35, 5, "[[[1,10,1]1,20,2]1,30,1[[1,35,1]1,40,1[1,50,1]]]"); // Case 3
+        TestInsert(tree, 55, 3, "[[[1,10,1]1,20,2]2,30,1[[1,35,1]2,40,1[2,50,1[1,55,1]]]]"); // Case 1
+        TestInsert(tree, 45, 0, "[[[1,10,1]1,20,2]2,30,1[[1,35,1]2,40,1[[1,45,1]1,50,1[1,55,1]]]]");
+        TestInsert(tree, 32, 1, "[[[1,10,1]1,20,2]2,30,1[[[1,32,1]1,35,2]1,40,1[[1,45,1]1,50,1[1,55,1]]]]"); // Case 1
+        EndTestSuite();
+
+        StartTestSuite("Delete 2");
+        TestDelete(tree, 35, 0, "[[[1,10,1]1,20,2]2,30,1[[1,32,1]2,40,1[[1,45,1]1,50,1[1,55,1]]]]");
+        TestDelete(tree, 50, 0, "[[[1,10,1]1,20,2]2,30,1[[1,32,1]2,40,1[[1,45,1]1,55,2]]]");
         TestDelete(tree, 45, 0, "[[[1,10,1]1,20,2]2,30,1[[1,32,1]2,40,2[1,55,1]]]");
         TestDelete(tree, 10, 3, "[[1,20,1]2,30,1[[1,32,1]1,40,1[1,55,1]]]"); // Case 2
         TestDelete(tree, 32, 0, "[[1,20,1]2,30,1[2,40,1[1,55,1]]]");
@@ -43,8 +63,25 @@ public class TestTree {
         TestInsert(tree, 47, 1, "[[1,30,1]2,40,1[[1,47,1]1,55,2]]");
         TestDelete(tree, 30, 3, "[[1,40,1]2,47,2[1,55,1]]"); // Case 4
         TestDelete(tree, 55, 3, "[[1,40,1]1,47,2]"); // Case 1
-        System.out.println(tree);
+        TestDelete(tree, 47, 3, "[1,40,1]"); // Case 1);
+        EndTestSuite();
         System.out.println("Done!");
+    }
+
+    static void StartTestSuite(String name) {
+        testSuiteName = name;
+        testSuiteFailed = false;
+        testSuiteAborted = false;
+        testNum = 1;
+    }
+
+    static void EndTestSuite() {
+        if (testSuiteFailed) {
+            System.out.println("End test suite: " + testSuiteName + "\n");
+        }
+        else {
+            System.out.println("Test suite " + testSuiteName + " finished with no errors\n");
+        }
     }
 
     static void TestInsert(WAVLTree tree, int key, int rebalancingOperations, String expected) {
@@ -52,45 +89,43 @@ public class TestTree {
     }
 
     static void TestInsert(WAVLTree tree, int key, int rebalancingOperations, String expected, boolean verbose) {
+        if (testSuiteAborted) {
+            return;
+        }
+        StringBuilder message = new StringBuilder();
+        boolean failed = false;
         int numOperations;
         try {
             numOperations = tree.insert(key, "");
-        } catch (RuntimeException rte) {
-            System.err.println("[Error " + String.valueOf(testNum) + "]: Exception was " +
-                    "thrown during run of insert(" + String.valueOf(key) + ").\n" +
-                    "Message:\n" + rte.toString());
-            throw rte;
         }
-        if (numOperations != rebalancingOperations && (VERBOSE || verbose)) {
-            System.err.println("[Warning " + String.valueOf(testNum) + "]: Number of rebalancing " +
-                    "operations (" + String.valueOf(numOperations) +
-                    ") in insert(" + String.valueOf(key) + ") does not match " +
-                    "expected value (" + String.valueOf(rebalancingOperations) + ").");
-        }
-        String result = tree.toString();
-        int length = Math.min(result.length(), expected.length());
-        char[] diff = new char[length];
-        boolean different = false;
-        for (int i = 0; i < length; i++) {
-            if (result.charAt(i) != expected.charAt(i)) {
-                diff[i] = '^';
-                different = true;
+        catch (RuntimeException rte) {
+            if (!testSuiteFailed) {
+                System.out.print(testSuiteName + ":");
+                testSuiteFailed = true;
             }
-            else {
-                diff[i] = ' ';
-            }
+            System.out.println(errorWasThrown(Level.ERROR, rte, String.format("insert(%d)", key)));
+            testSuiteAborted = true;
+            return;
         }
-        if (different || result.length() != expected.length()) {
-            String message = "[Error " + String.valueOf(testNum) + "]: Resulting tree " +
-                    "after insert(" + String.valueOf(key) + ") is incorrect!\n\t\t" +
-                    result + "\n\t\t" + String.valueOf(diff) + "\n\t\t" +
-                    expected;
+        if ((VERBOSE || verbose) &&
+                !isValueCorrect(Level.WARNING, "Number of rebalancing operations", String.format("insert(%d)", key),
+                rebalancingOperations, numOperations, message)) {
+            failed = true;
+            message.append(String.format("%n"));
+        }
+        if (!isTreeCorrect(Level.ERROR, String.format("insert(%d)", key), expected, tree.toString(), message)) {
+            failed = true;
             if (ERROR_ON_MISTAKE) {
-                throw new RuntimeException(message);
+                testSuiteAborted = true;
             }
-            else {
-                System.err.println(message);
+            message.append(String.format("%n"));
+        }
+        if (failed) {
+            if (!testSuiteFailed) {
+                message.insert(0, String.format("%s:%n", testSuiteName));
+                testSuiteFailed = true;
             }
+            System.out.print(message);
         }
         testNum++;
     }
@@ -100,23 +135,64 @@ public class TestTree {
     }
 
     static void TestDelete(WAVLTree tree, int key, int rebalancingOperations, String expected, boolean verbose) {
+        if (testSuiteAborted) {
+            return;
+        }
+        StringBuilder message = new StringBuilder();
+        boolean failed = false;
         int numOperations;
         try {
             numOperations = tree.delete(key);
         }
         catch (RuntimeException rte) {
-            System.err.println("[Error " + String.valueOf(testNum) + "]: Exception was " +
-                    "thrown during run of delete(" + String.valueOf(key) + ").\n" +
-                    "Message:\n" + rte.toString());
-            throw rte;
+            if (!testSuiteFailed) {
+                System.out.println(testSuiteName + ":");
+                testSuiteFailed = true;
+            }
+            System.out.println(errorWasThrown(Level.ERROR, rte, String.format("delete(%d)", key)));
+            testSuiteAborted = true;
+            return;
         }
-        if (numOperations != rebalancingOperations && (VERBOSE || verbose)) {
-            System.err.println("[Warning " + String.valueOf(testNum) + "]: Number of rebalancing " +
-                    "operations (" + String.valueOf(numOperations) +
-                    ") in delete(" + String.valueOf(key) + ") does not match " +
-                    "expected value (" + String.valueOf(rebalancingOperations) + ").");
+        if ((VERBOSE || verbose) &&
+                !isValueCorrect(Level.WARNING, "Number of rebalancing operations", String.format("delete(%d)", key),
+                        rebalancingOperations, numOperations, message)) {
+            failed = true;
+            message.append(String.format("%n"));
         }
-        String result = tree.toString();
+        if (!isTreeCorrect(Level.ERROR, String.format("delete(%d)", key), expected, tree.toString(), message)) {
+            failed = true;
+            if (ERROR_ON_MISTAKE) {
+                testSuiteAborted = true;
+            }
+            message.append(String.format("%n"));
+        }
+        if (failed) {
+            if (!testSuiteFailed) {
+                message.insert(0, String.format("%s:%n", testSuiteName));
+                testSuiteFailed = true;
+            }
+            System.out.print(message);
+        }
+        testNum++;
+    }
+
+    static StringBuilder errorWasThrown(Level level, RuntimeException rte, String runOf) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("[%s %d]: Exception was thrown during run of '%s'.%n",
+                level, testNum, runOf));
+        builder.append(rte);
+        return builder;
+    }
+    static <T> boolean isValueCorrect(Level level, String valueName, String in, T expected, T result,
+                                                       StringBuilder message) {
+        if (!expected.equals(result)) {
+            message.append(String.format("[%s %d]: %s in %s does not match! expected %s, got %s",
+                    level, testNum, valueName, in, expected, result));
+            return false;
+        }
+        return true;
+    }
+    static boolean isTreeCorrect(Level level, String after, String expected, String result, StringBuilder message) {
         int length = Math.min(result.length(), expected.length());
         char[] diff = new char[length];
         boolean different = false;
@@ -130,18 +206,26 @@ public class TestTree {
             }
         }
         if (different || result.length() != expected.length()) {
-            String message = "[Error " + String.valueOf(testNum) + "]: Resulting tree " +
-                    "after delete(" + String.valueOf(key) + ") is incorrect!\n\t\t" +
-                    result + "\n\t\t" + String.valueOf(diff) + "\n\t\t" +
-                    expected;
-            if (ERROR_ON_MISTAKE) {
-                throw new RuntimeException(message);
-            }
-            else {
-                System.err.println(message);
-            }
+            message.append(String.format("[%s %d]: ", level, testNum));
+            message.append(String.format("Resulting tree after %s is incorrect%n\t\t", after));
+            message.append(String.format("%s%n\t\t%s%n\t\t%s", result, String.valueOf(diff), expected));
+            return false;
         }
-        testNum++;
+        return true;
+    }
+
+    private enum Level {
+        //INFO("Info"),
+        WARNING("Warning"),
+        ERROR("Error");
+        private String text;
+        private Level(String text) {
+            this.text = text;
+        }
+        @Override
+        public String toString() {
+            return text;
+        }
     }
 }
 
