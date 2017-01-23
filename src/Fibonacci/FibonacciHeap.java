7efaac5fb31acf0f5e7558bfa0a1ea4060d3b9e2
Saar Katz <kats.saar@gmail.com>
2017-01-23 14:17:34 +0200

    Added link, onePassSuccesiveLinking, meld, removeNodeFromList. Some fixes.

--- src/Fibonacci/FibonacciHeap.java
+++ src/Fibonacci/FibonacciHeap.java
@@ -6,15 +6,20 @@ package Fibonacci;
  * An implementation of fibonacci heap over non-negative integers.
  */
 public class FibonacciHeap {
-    static int totalCuts;
-    static int totalLinks;
+    private static int totalCuts = 0;
+    private static int totalLinks = 0;
 
-    int size;
-    HeapNode min;
+    private int numMarks;
+    private int numRoots;
+
+    private int size;
+    private HeapNode min;
 
     FibonacciHeap() {
         size = 0;
         min = null;
+        numRoots = 0;
+        numMarks = 0;
     }
 
    /**
@@ -36,7 +41,15 @@ public class FibonacciHeap {
     * Creates a node (of type HeapNode) which contains the given key, and inserts it into the heap. 
     */
     public HeapNode insert(int key) {
-    	return null;//new HeapNode(); // should be replaced by student code
+        HeapNode newNode = new HeapNode(key);
+        HeapNode minNode = findMin();
+        meld(newNode, minNode);
+        if (newNode.getKey() < minNode.getKey()) {
+           min = newNode;
+        }
+        numRoots++;
+        size++;
+        return newNode;
     }
 
    /**
@@ -45,7 +58,9 @@ public class FibonacciHeap {
     * Delete the node containing the minimum key.
     *
     */
-    public void deleteMin() { delete(findMin()); }
+    public void deleteMin() {
+        delete(findMin());
+    }
 
    /**
     * public HeapNode findMin()
@@ -64,7 +79,23 @@ public class FibonacciHeap {
     *
     */
     public void meld (FibonacciHeap heap2) {
-    	  return; // TODO should be replaced by student code
+        HeapNode minNode2 = heap2.findMin();
+        HeapNode minNode = findMin();
+        meld(minNode2, minNode);
+
+        // Keep all members updated.
+        if (minNode2.getKey() < minNode.getKey()) {
+            min = minNode2;
+        }
+        this.size = this.size() + heap2.size();
+        this.numRoots = this.numRoots + heap2.numRoots;
+        this.numMarks = this.numMarks + heap2.numMarks;
+
+        // Empty heap2
+        heap2.size = 0;
+        heap2.min = null;
+        heap2.numRoots = 0;
+        heap2.numMarks = 0;
     }
 
    /**
@@ -84,8 +115,13 @@ public class FibonacciHeap {
     * 
     */
     public int[] countersRep() {
-	    int[] arr = new int[42];
-        return arr; //	 to be replaced by student code
+	    int[] arr = new int[(int)Math.ceil(Math.log(size())/Math.log(2))];
+        HeapNode node = findMin();
+        for (int i = 0; i < numRoots; i++) {
+            arr[node.getRank()]++;
+            node = node.getNext();
+        }
+        return arr;
     }
 	
    /**
@@ -95,36 +131,32 @@ public class FibonacciHeap {
     *
     */
     public void delete(HeapNode x) {
-        if (x.equals(min)) {
-            //updating min is required and we know the new min is one of the other roots.
-            HeapNode node = x.getNext();
-            min = node;
-            while (!x.equals(node)) {
-                //iterating over the roots and updating the new min.
-                if (node.getKey() < min.getKey()) {
-                    min = node;
-                }
-                node = node.getNext();
+        if (size() == 1) {
+            min = null;
+            numRoots--;
+            return;
+        }
+        if (x.getParent() != null) {
+            cascadingCutCounting(x, false);
+        }
+        while (x.getChild() != null) {
+            cutWithoutCounting(x.getChild());
+        }
+        min = x.getNext(); // In case x was min
+        removeNodeFromList(x);
+        numRoots--;
+
+        onePassSuccessiveLinking();
+
+        // Find min again.
+        x = min;
+        HeapNode node = min.getNext();
+        while (!node.equals(x)) {
+            //iterating over the roots and updating the new min.
+            if (node.getKey() < min.getKey()) {
+                min = node;
             }
-        }
-        HeapNode child = x.getChild();
-        HeapNode node = child.getNext();
-        //cascading cuts for all the children of x.
-        cascadingCut(child);
-        while (!node.equals(child)) {
-            cascadingCut(node);
             node = node.getNext();
-        }
-        //now x has no children, only left to disconnect it from the heap.
-        x.setParent(null);
-        x.getParent().setRank(x.getParent().getRank() - 1);
-        if (x.getNext().equals(x)) {
-            x.getParent().setChild(null);
-        }
-        else {
-            x.getParent().setChild(x.getNext());
-            x.getPrev().setNext(x.getNext());
-            x.getNext().setPrev(x.getPrev());
         }
     }
 
@@ -135,7 +167,11 @@ public class FibonacciHeap {
     * to reflect this chage (for example, the cascading cuts procedure should be applied if needed).
     */
     public void decreaseKey(HeapNode x, int delta) {
-    	return; // should be replaced by student code
+    	x.setRank(x.getRank() - delta);
+        if(x.getParent() != null && x.getKey() < x.getParent().getKey()) {
+            cascadingCut(x);
+        }
+        onePassSuccessiveLinking();
     }
 
    /**
@@ -146,7 +182,7 @@ public class FibonacciHeap {
     * The potential equals to the number of trees in the heap plus twice the number of marked nodes in the heap. 
     */
     public int potential() {
-    	return 0; // should be replaced by student code
+    	return numRoots + 2*numMarks;
     }
 
    /**
@@ -158,19 +194,141 @@ public class FibonacciHeap {
     * in its root.
     */
     public static int totalLinks() {
-    	return totalLinks; // should be replaced by student code
+    	return totalLinks;
     }
 
    /**
     * public static int totalCuts() 
     *
     * This static function returns the total number of cut operations made during the run-time of the program.
-    * A cut operation is the operation which diconnects a subtree from its parent (during decreaseKey/delete methods). 
+    * A cut operation is the operation which disconnects a subtree from its parent (during decreaseKey/delete methods).
     */
     public static int totalCuts() {
-    	return totalCuts; // should be replaced by student code
+    	return totalCuts;
     }
-    
+
+    private void cutWithoutCounting(HeapNode x) {
+        HeapNode parent = x.getParent();
+        x.setParent(null);
+        if (x.isMark()) {
+            x.setMark(false);
+            numMarks--;
+        }
+        parent.setRank(parent.getRank() - 1);
+        if (x.getNext().equals(x)) {
+            parent.setChild(null);
+        }
+        else {
+            parent.setChild(x.getNext());
+            removeNodeFromList(x);
+        }
+        meld(x, findMin());
+        numRoots++;
+    }
+
+    private void cut(HeapNode x) {
+        cutWithoutCounting(x);
+        totalCuts++;
+    }
+    private void cascadingCutCounting(HeapNode x, boolean doCount) {
+        HeapNode parent = x.getParent();
+        if (doCount) {
+            cut(x);
+        }
+        else {
+            cutWithoutCounting(x);
+        }
+        if (parent.getParent() != null) {
+            if (parent.isMark() == false) {
+                parent.setMark(true);
+                numMarks++;
+            }
+            else {
+                cascadingCutCounting(parent, doCount);
+            }
+        }
+    }
+    private void cascadingCut(HeapNode x) {
+        cascadingCutCounting(x, true);
+    }
+
+    private HeapNode link(HeapNode node1, HeapNode node2) {
+        if (node1.getRank() != node2.getRank()) {
+            throw new RuntimeException("Attempting to link nodes of different ranks!");
+        }
+        if (node1.getKey() > node2.getKey()) {
+            HeapNode temp = node1;
+            node1 = node2;
+            node2 = temp;
+        }
+        if (node1.getChild() != null) {
+            meld(node1.getChild(), node2);
+        }
+        node1.setChild(node2);
+        node2.setParent(node1);
+        node1.setRank(node1.getRank() + 1);
+        totalLinks++;
+        return node1;
+    }
+
+    private void onePassSuccessiveLinking() {
+        int arraySize = (int)(1.44 * Math.ceil(Math.log(size())/Math.log(2)) + 1); // ceil(log_2(size))
+        HeapNode node = findMin();
+        HeapNode prev;
+        HeapNode heapArray[] = new HeapNode[arraySize];
+        HeapNode heapOut = null;
+        int initialNimRoots = numRoots;
+        for (int i = 0; i < initialNimRoots; i++) {
+            // Check if there is already a node with the same rank in the array
+            if (heapArray[node.getRank()] == null) {
+                heapArray[node.getRank()] = node;
+                node = node.getNext();
+            }
+            // If it is the link it with the existing one one move them to the result
+            else {
+                prev = node;
+                node = node.getNext();
+                removeNodeFromList(prev);
+                removeNodeFromList(heapArray[prev.getRank()]);
+                prev = link(prev, heapArray[prev.getRank()]);
+
+                // Add the linked node to the result
+                if (heapOut == null) {
+                    heapOut = prev;
+                }
+                else {
+                    meld(heapOut, prev);
+                }
+                numRoots--;
+                heapArray[node.getRank()] = null;
+            }
+        }
+        if (heapOut != null) {
+            for (int i = 0; i < arraySize; i++) {
+                if (heapArray[i] != null) {
+                    meld(heapOut, heapArray[i]);
+                    break;
+                }
+            }
+            min = heapOut;
+        }
+    }
+
+    private void meld(HeapNode first, HeapNode second) {
+        first.getPrev().setNext(second);
+        second.getPrev().setNext(first);
+        HeapNode firstPrev = first.getPrev();
+        first.setPrev(second.getPrev());
+        second.setPrev(firstPrev);
+    }
+
+    private void removeNodeFromList(HeapNode node) {
+        node.getNext().setPrev(node.getPrev());
+        node.getPrev().setNext(node.getNext());
+        node.setNext(node);
+        node.setPrev(node);
+    }
+
     /**
      * public class HeapNode
      *
@@ -179,35 +337,7 @@ public class FibonacciHeap {
      * another file
      *
      */
-    private void cut(HeapNode x) {
-        HeapNode parent = x.getParent();
-        x.setParent(null);
-        x.setMark(false);
-        parent.setRank(parent.getRank() - 1);
-        if (x.getNext().equals(x)) {
-            parent.setChild(null);
-        }
-        else {
-            parent.setChild(x.next);
-            x.getPrev().setNext(x.getNext());
-            x.getNext().setPrev(x.getPrev());
-        }
-        totalCuts++;
-    }
-    private void cascadingCut(HeapNode x) {
-        HeapNode parent = x.getParent();
-        cut(x);
-        if (parent.getParent() != null) {
-            if (parent.isMark() == false) {
-                parent.setMark(true);
-            }
-            else {
-                cascadingCut(parent);
-            }
-        }
-    }
-
-    public class HeapNode{
+    public class HeapNode {
         private String value;
         private int key;
         private int rank;
@@ -217,7 +347,7 @@ public class FibonacciHeap {
         private HeapNode prev;
         private HeapNode parent;
 
-        public HeapNode(int key, String value) {
+        private HeapNode(int key, String value) {
             this.value = value;
             this.key = key;
             rank = 0;
@@ -226,6 +356,10 @@ public class FibonacciHeap {
             next = null;
             prev = null;
             parent = null;
+        }
+
+        private HeapNode(int key) {
+            this(key, null);
         }
 
         private boolean isMark() {
